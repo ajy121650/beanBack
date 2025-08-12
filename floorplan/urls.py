22d500de1beb5652d99586f3b69708215c97832a
ajy1216 <ajy1216@snu.ac.kr>
2025-08-13 02:14:01 +0900

    Fix url-name

--- floorplan/urls.py
+++ floorplan/urls.py
@@ -6,7 +6,7 @@ app_name = 'floorplans'
 urlpatterns = [
     path("", FloorPlanListView.as_view(), name='list'),
     path("<int:floorplan_id>/", FloorPlanDetailView.as_view(), name='detail'),
-    path("by-owner/<int:owner_id>/", FloorPlanOwnerView.as_view(), name='owner'),
-    path("by-cafe/<int:cafe_id>/", FloorPlanCafeView.as_view(), name='cafe'),
+    path("by-owner/<int:owner_id>/", FloorPlanOwnerView.as_view(), name='by-owner'),
+    path("by-cafe/<int:cafe_id>/", FloorPlanCafeView.as_view(), name='by-cafe'),
     path("detection/", FloorPlanDetectionView.as_view(), name='detection'),
 ]
