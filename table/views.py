5976330e329f9474d42ce477eb9a3703a61d4831
eileen <eileen914@snu.ac.kr>
2025-08-22 13:24:05 +0900

    [STYLE] 코드 스타일 수정

--- table/views.py
+++ table/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
