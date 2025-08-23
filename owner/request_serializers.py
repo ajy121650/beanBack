420054e234c097e9f6a0d4fef65f454ad3e4784b
cho58 <oke08034@snu.ac.kr>
2025-08-23 15:37:52 +0900

    comments

--- owner/request_serializers.py
+++ owner/request_serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 
-
+# 회원가입 요청 시 필요한 데이터(username, password)를 검증하는 Serializer
 class SignUpRequestSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
 
+# 로그인 요청 시 필요한 데이터(username, password)를 검증하는 Serializer
 class SignInRequestSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
 
+# refresh token을 검증하는 Serializer
 class TokenRefreshRequestSerializer(serializers.Serializer):
     refresh = serializers.CharField()
 
