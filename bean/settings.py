004901ebd8ce8d2c1d09332ecb3882cdeb28daea
ajy1216 <ajy1216@snu.ac.kr>
2025-08-25 00:07:08 +0900

    Feat deploy settings

--- bean/settings.py
+++ bean/settings.py
@@ -88,9 +88,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bean.wsgi.application'
 
 DATABASES = {
-		'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+		 "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_USER"),
+            "PASSWORD": os.environ.get("DB_PASSWORD"),
+            "HOST": os.environ.get("DB_HOST"),
+            "PORT": int(os.environ.get("DB_PORT", "3306")),
         }
 }
 
